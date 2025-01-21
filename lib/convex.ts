b9b77accd17f593582c22d4588aaad9496d329b1
saidurpulok <saidurr13@gmail.com>
2025-01-21 21:51:13 +0600

    error when env not set

--- lib/convex.ts
+++ lib/convex.ts
@@ -2,5 +2,10 @@ import { ConvexHttpClient } from "convex/browser";
 
 // Create a singleton instance of the Convex HTTP client
 export const getConvexClient = () => {
-  return new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+  if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
+    throw new Error('NEXT_PUBLIC_CONVEX_URL environment variable is not set');
+  }
+  
+  const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL;
+  return new ConvexHttpClient(convexUrl);
 };
